6a8bb7baa5287f72d9fdfda3c04def6a6c817c65
jearbcpe <teerapon.jearb@gmail.com>
2020-01-24 00:49:42 +0700

    add authen

--- service.php
+++ service.php
@@ -2,6 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 //header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+date_default_timezone_set("Asia/Bangkok");
 
 require 'connect_db.php';
 require '../vendor/autoload.php';
@@ -108,6 +109,93 @@ $app->post('/editUser', function ($request, $response, $args) use ($app) {
 		echo json_encode(array('status' => 'success'));
 	else
 		echo json_encode(array('status' => 'fail'));
+
+});	
+
+$app->post('/verifyUser', function ($request, $response, $args) use ($app) {
+
+	$json = $request->getBody();
+	$data = json_decode($json, true); 
+	$username = $data['username'];
+	$password = $data['password'];
+
+	$userFullName = "";
+	$conn = conndb();
+	$sql = "select u.userId,u.username,u.fullName ";
+	$sql .= "from users u ";
+	$sql .= "where u.username = '".$username."' and u.password = MD5('".$password."') and u.flag='1'";
+		
+	$query = $conn->query($sql);
+
+	if($row = $query->fetch_assoc())
+	{
+		$tokenStr = md5($row['username'].date('Y-m-d H:i:s'));
+		$userId = $row['userId'];
+		$fullName = $row['fullName'];
+		$conn = conndb();
+		$sql = "insert into userToken(userId,tokenStr,createDT,lastActive) ";
+		$sql .= "values(".$userId.",'".$tokenStr."','".date('Y-m-d H:i:s')."','".date('Y-m-d H:i:s')."')";
+
+		if($conn->query($sql))
+		{
+			$sql = "update userToken set flag='0' where userId=".$userId." and tokenStr != '".$tokenStr."' ";
+			$conn->query($sql);
+
+			echo json_encode(array('status' => 'success','token' =>$tokenStr ,'fullName' =>  $fullName));
+		}
+		else
+			echo json_encode(array('status' => 'fail','token' =>''));
+	}
+	else
+		echo json_encode(array('status' => 'fail','token' =>''));
+
+});	
+
+$app->post('/verifyToken', function ($request, $response, $args) use ($app) {
+
+	$json = $request->getBody();
+	$data = json_decode($json, true); 
+	$token = $data['token'];
+	$diffTime = "";
+	$pass = false;
+
+	$conn = conndb();
+	$sql = "select ROUND(time_to_sec((TIMEDIFF(NOW(), lastActive))) / 60) as diffTime from userToken where tokenStr = '".$token."' and flag = '1'";
+		
+	$query = $conn->query($sql);
+
+	if($row = $query->fetch_assoc())
+		$diffTime = $row['diffTime'];
+
+	if($diffTime != "")
+	{
+		if(intval($diffTime) < 30 )
+		{		
+			$pass = true;
+			$sql = "update userToken set lastActive='".date('Y-m-d H:i:s')."' where tokenStr = '".$token."' ";
+			$conn->query($sql);
+		}		
+	}
+
+	if($pass) 		
+		echo json_encode(array('status' => 'success'));
+	else
+		echo json_encode(array('status' => 'expired'));		
+});	
+
+$app->post('/logout', function ($request, $response, $args) use ($app) {
+
+	$json = $request->getBody();
+	$data = json_decode($json, true); 
+
+	$conn = conndb();
+	$sql = "update userToken set flag='0' where tokenStr = '".$data['token']."' ";
+
+	if($conn->query($sql))
+		echo json_encode(array('status' => 'success'));
+	else
+		echo json_encode(array('status' => 'fail'));
+		
 
 });	
 
